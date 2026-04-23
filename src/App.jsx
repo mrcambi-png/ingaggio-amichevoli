c47e78a0bbe277db3770c76e96644c09c6172ba4
mrcambi-png <mrcambi@gmail.com>
2026-04-23 11:21:33 +0200

    Partenza

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center', fontFamily: 'sans-serif' }}>
+      <h1>🚀 Benvenuto su Ingaggio</h1>
+      <p>Se vedi questa scritta, il tuo computer è configurato correttamente!</p>
+      <div style={{ marginTop: '30px', padding: '20px', border: '2px solid green', borderRadius: '15px', display: 'inline-block' }}>
+        <b style={{ color: 'green' }}>✅ CONNESSIONE PRONTA</b>
+      </div>
+    </div>
+  )
+}
+export default App
